68b4102375207224ea0308cac0a7706ec10393fe
Priscilla Jiménez <pjoselyn2114@gmail.com>
2025-06-19 15:59:39 -0600

    Track load status

--- workers/matrix/main.go
+++ workers/matrix/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"os"
 	"strconv"
+	"sync/atomic"
 
 	"github.com/KateGF/Http-Server-Project-SO/core"
 )
@@ -13,15 +14,28 @@ func mustAtoi(s string) int {
 }
 
 func main() {
+	var currentLoad int64
+	var doneCount int64
+
 	srv := core.NewHttpServer()
 
 	srv.Get("/ping", func(req *core.HttpRequest) (*core.HttpResponse, error) {
 		return core.Ok().JsonObj(map[string]any{
-			"status": "ok", "load": 0, "done": 0,
+			"status": "ok",
+			"load":   atomic.LoadInt64(&currentLoad),
+			"done":   atomic.LoadInt64(&doneCount),
 		}), nil
 	})
 
-	srv.Post("/matrix", MatrixHandler)
+	srv.Post("/matrix", func(req *core.HttpRequest) (*core.HttpResponse, error) {
+		atomic.AddInt64(&currentLoad, 1)
+		defer atomic.AddInt64(&currentLoad, -1)
+		response, err := MatrixHandler(req)
+		if err == nil && response.StatusCode == 200 {
+			atomic.AddInt64(&doneCount, 1)
+		}
+		return response, err
+	})
 
 	port := mustAtoi(os.Getenv("PORT"))
 	srv.Start(port)
